2cfcd52523c0c458c33c00135abd0d200c784a1f
Lucas Bomfim <137433770+lucasbomfim15@users.noreply.github.com>
2025-02-13 23:01:28 +0000

    refactor controller

--- atv/src/main/java/com/atv1/atv/HelloController.java
+++ atv/src/main/java/com/atv1/atv/HelloController.java
@@ -1,3 +1,6 @@
+package com.atv1.atv;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
