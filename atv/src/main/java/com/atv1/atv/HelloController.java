f561654d76fe6c87ef4c4ed0f7eec6f42e81e553
Lucas Bomfim <137433770+lucasbomfim15@users.noreply.github.com>
2025-02-13 22:37:39 +0000

    refactor controller

--- atv/src/main/java/com/atv1/atv/HelloController.java
+++ atv/src/main/java/com/atv1/atv/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello ";
     }
